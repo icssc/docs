3de7e3d328eeee8d92ffb892c23dddc64786d1c4
Alexander Liu <a@alexanderliu.com>
2024-10-03 14:34:37 -0700

    feat: ✨ update homepage

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,19 +1,30 @@
-import Link from "next/link";
+import { Card, Cards } from "fumadocs-ui/components/card";
+import { Brush, BookOpenText, ScrollText } from "lucide-react";
 
 export default function HomePage() {
   return (
-    <main className="flex flex-col justify-center h-screen text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
-        You can open{" "}
-        <Link
+    <main className="p-4">
+      <h1 className="mt-6 mb-4 text-2xl font-medium">Welcome to ICSSC</h1>
+      <Cards>
+        <Card
+          icon={<Brush />}
+          href="/docs/brand"
+          title="Brand Guide"
+          description="Logos, colors, topography, guidelines"
+        />
+        <Card
+          icon={<BookOpenText />}
           href="/docs/developer"
-          className="font-semibold underline text-fd-foreground"
-        >
-          /docs/developer
-        </Link>{" "}
-        and see the documentation.
-      </p>
+          title="Developer Docs"
+          description="Public facing developer projects"
+        />
+        <Card
+          icon={<ScrollText />}
+          href="/docs/contributor"
+          title="Contributor Docs"
+          description="Internal documentation and design reasoning"
+        />
+      </Cards>
     </main>
   );
 }
