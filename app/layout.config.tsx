172a963a0aaf4a8ae27bf08256e50dc14a6b1a6f
Alexander Liu <a@alexanderliu.com>
2024-10-28 22:02:01 -0700

    fix: 🐛 home layout import path

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,4 +1,4 @@
-import type { HomeLayoutProps } from "fumadocs-ui/home-layout";
+import type { HomeLayoutProps } from "fumadocs-ui/layouts/home";
 
 import { ICSSC } from "@/components/logos/ICSSC";
 
