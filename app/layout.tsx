aa8cf10429fd14c55ace16cbb708b17ef4adfff9
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2024-11-19 16:49:26 -0800

    feat: ✨ fix twoslash, finish ts-integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import "./global.css";
 import "fumadocs-twoslash/twoslash.css";
+import "./global.css";
 import { RootProvider } from "fumadocs-ui/provider";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
--- source.config.ts
+++ source.config.ts
@@ -1,5 +1,17 @@
+import { rehypeCodeDefaultOptions } from "fumadocs-core/mdx-plugins";
 import { defineDocs, defineConfig } from "fumadocs-mdx/config";
+import { transformerTwoslash } from "fumadocs-twoslash";
 
 export const { docs, meta } = defineDocs();
 
-export default defineConfig();
+export default defineConfig({
+  mdxOptions: {
+    rehypeCodeOptions: {
+      themes: {
+        light: "catppuccin-latte",
+        dark: "catppuccin-mocha",
+      },
+      transformers: [...(rehypeCodeDefaultOptions.transformers ?? []), transformerTwoslash()],
+    },
+  },
+});
